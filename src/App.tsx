41825b6ffb43ac6556dee9a9d12e2014a2f90fdd
Rhea <101016117+StringRays@users.noreply.github.com>
2022-12-28 18:10:46 -0600

    styling and can display 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+interface quoteProps {
+  quote: string;
+  author: string
+}
 
+function Quote(props: quoteProps) {
+  return (
+    <div className="quote-wrapper">
+        <div id="text">"{props.quote}"</div>
+        <div id="author">- {props.author}</div>
+    </div>
+  )
+}
 
 function App() {
   const [quotes, setQuotes] = useState({
@@ -12,36 +24,38 @@ function App() {
     currentQuoteAuthor: ""
   });
   const [color, setColor] = useState("#01949A");
+  const [amount, setAmount] = useState(1);
+  
 
   const fetchQuotes = () => {
     fetch('https://type.fit/api/quotes')
-        .then(async response => {
-            const data = await response.json();
-            if (!response.ok) {
-                const error = (data && data.message) || response.statusText;
-                return Promise.reject(error);
-            }
-            //This gives us a list of many quotes to cycle through, some have the author property empty
-            let newIndex: number = Math.floor(Math.random()*data.length);
-            let newAuthor: string = data[newIndex].author ? data[newIndex].author : "Anonymous";
-            setQuotes(() => { 
-              return { 
-                quoteList: data, 
-                currentQuoteText: data[newIndex].text, 
-                currentQuoteAuthor: newAuthor
-              }
-            });
-        })
-        .catch(error => {
-            console.error('There was an error!', error);
+      .then(async (response) => {
+        const data = await response.json();
+        if (!response.ok) {
+          const error = (data && data.message) || response.statusText;
+          return Promise.reject(error);
+        }
+        //This gives us a list of many quotes to cycle through, some have the author property empty
+        let newIndex: number = Math.floor(Math.random() * data.length);
+        let newAuthor: string = data[newIndex].author ? data[newIndex].author : "Unknown";
+        setQuotes(() => {
+          return {
+            quoteList: data,
+            currentQuoteText: data[newIndex].text,
+            currentQuoteAuthor: newAuthor
+          };
         });
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
   }
 
     const updateQuote = () => {
       let newIndex: number = Math.floor(Math.random()*quotes.quoteList.length);
       let newQuote = quotes.quoteList[newIndex];
       if (newQuote.author === null){
-        newQuote.author = "Anonymous";
+        newQuote.author = "Unknown";
       }
       setQuotes(previousState => { 
         return { 
@@ -56,7 +70,10 @@ function App() {
       color==="#01949A" ? setColor("#004369") : color==="#004369" ? setColor("#DB1F48") : setColor("#01949A");
     }
 
-  //This runs twice in dev mode but shouldn't in production build
+    const changeAmount = () => {
+      amount === 1 ? setAmount(10) : setAmount(1);
+    }
+
   useEffect(() => {
     fetchQuotes();
   }, [])
@@ -65,12 +82,62 @@ function App() {
     <div className="App" style={{ backgroundColor: color }}>
       <div className="quote-box">
         <h1 style={{ color: color }}>Random Quote Generator</h1>
-        <div id="text">"{quotes.currentQuoteText}"</div>
-        <div id="author">- {quotes.currentQuoteAuthor}</div>
-        <div id="buttons">
-          <button onClick={updateQuote}>New Quote!</button>
-          <button onClick={changeColor}>Change Color!</button>
+        {amount===1 ? (
+          <div id='wrapper'>
+            <Quote quote={quotes.currentQuoteText} author={quotes.currentQuoteAuthor} />
+            <div id="buttons">
+              <button 
+                style={{ color: color, borderColor: color, boxShadow: "2px 2px 10px "+color+" inset, -2px -2px 10px "+color+" inset" }}
+                onClick={updateQuote}>
+                  New Quote{amount > 1 && "s"}!
+                </button>
+              <button
+                style={{ color: color, borderColor: color, boxShadow: "2px 2px 10px "+color+" inset, -2px -2px 10px "+color+" inset" }}
+                onClick={changeColor}>
+                  Change Color!
+              </button>
+              <button
+                style={{ color: color, borderColor: color, boxShadow: "2px 2px 10px "+color+" inset, -2px -2px 10px "+color+" inset" }}
+                onClick={changeAmount}>
+                  {amount===1 ? (
+                    'More Quotes?'
+                  ) : (
+                    'Fewer?'
+                  )}
+              </button>
+            </div>
+          </div>
+        ) : (
+        <div id="wrapper">
+          <div id="buttons">
+            <button 
+              style={{ color: color, borderColor: color, boxShadow: "2px 2px 10px "+color+" inset, -2px -2px 10px "+color+" inset" }}
+              onClick={updateQuote}>
+                New Quote{amount > 1 && "s"}!
+              </button>
+            <button
+              style={{ color: color, borderColor: color, boxShadow: "2px 2px 10px "+color+" inset, -2px -2px 10px "+color+" inset" }}
+              onClick={changeColor}>
+                Change Color!
+            </button>
+            <button
+              style={{ color: color, borderColor: color, boxShadow: "2px 2px 10px "+color+" inset, -2px -2px 10px "+color+" inset" }}
+              onClick={changeAmount}>
+                {amount===1 ? (
+                  'More Quotes?'
+                ) : (
+                  'Fewer?'
+                )}
+            </button>
+          </div>
+          {Array.from(Array(amount))
+            .map((_,i) => quotes.quoteList[i+Math.floor(Math.random()*(quotes.quoteList.length-amount))])
+            .map((quote) => quote.author ? quote : { text: quote.text, author: "Unknown" })
+            .map((quote, index) => (
+            <Quote key={index} quote={quote.text} author={quote.author} />
+        ))}
         </div>
+        )} 
       </div>
     </div>
   );
